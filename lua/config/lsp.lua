3e35497ad953200d4db867fd2788bebaa8b75daf
ezpcy <batu.s@live.com>
2025-04-03 14:57:57 +0200

    finally fixed java debugger

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,3 +1,4 @@
+require('java').setup()
 -- ================================================
 -- LSP SETTINGS
 -- ================================================
@@ -77,75 +78,10 @@ vim.g.markdown_fenced_languages = {
 }
 
 local lspconfig = require 'lspconfig'
-local jdtls = require 'jdtls'
-local home = os.getenv 'HOME'
-local workspace_dir = home .. '/.local/share/eclipse/' .. vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
-
-local root_dir = require('jdtls.setup').find_root {
-  '.git',
-  'mvnw',
-  'gradlew',
-  'pom.xml',
-  'build.gradle',
-} or vim.fn.getcwd()
-
-local bundles =
-  vim.split(vim.fn.glob(home .. '/.local/share/nvim/mason/packages/java-debug-adapter/extension/server/com.microsoft.java.debug.plugin-*.jar', 1), '\n')
-
-local config = {
-  cmd = {
-    'java',
-    '-Declipse.application=org.eclipse.jdt.ls.core.id1',
-    '-Dosgi.bundles.defaultStartLevel=4',
-    '-Declipse.product=org.eclipse.jdt.ls.core.product',
-    '-Dlog.protocol=true',
-    '-Dlog.level=ALL',
-    '-Xms1g',
-    '--add-modules=ALL-SYSTEM',
-    '--add-opens',
-    'java.base/java.util=ALL-UNNAMED',
-    '--add-opens',
-    'java.base/java.lang=ALL-UNNAMED',
-    '-jar',
-    vim.fn.glob(home .. '/.local/share/nvim/mason/packages/jdtls/plugins/org.eclipse.equinox.launcher_*.jar'),
-    '-configuration',
-    home .. '/.local/share/nvim/mason/packages/jdtls/config_linux',
-    '-data',
-    workspace_dir,
-  },
-
-  root_dir = root_dir,
-
-  init_options = {
-    bundles = bundles,
-  },
-
-  settings = {
-    java = {
-      project = {
-        referencedLibraries = {
-          '/usr/share/java/tomcat10/*',
-        },
-      },
-    },
-  },
-
-  on_attach = function(client, bufnr)
-    jdtls.setup_dap { hotcodereplace = 'auto' }
-    jdtls.dap.setup_dap_main_class_configs()
-  end,
-}
-
--- START the server:
-jdtls.start_or_attach(config)
 
 lspconfig.denols.setup {
   on_attach = on_attach,
   root_dir = lspconfig.util.root_pattern('deno.json', 'deno.jsonc'),
-}
-
-require('lspconfig').jdtls = {
-  autostart = false,
 }
 
 lspconfig.ts_ls.setup {
@@ -166,3 +102,5 @@ vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
     vim.bo.filetype = 'jsp'
   end,
 })
+
+lspconfig.jdtls.setup {}
