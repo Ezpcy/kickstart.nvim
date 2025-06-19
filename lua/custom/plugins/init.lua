4d1bacfbc120c1f30f0b3e6017f2b37b22604821
ezpcy <batu.s@live.com>
2025-06-19 15:28:44 +0200

    update: added auto session plugin

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -300,4 +300,16 @@ return {
   },
 
   { 'mfussenegger/nvim-jdtls', ft = 'java', dependencies = { 'nvim-dap' } },
+  {
+    'rmagatti/auto-session',
+    lazy = false,
+
+    ---enables autocomplete for opts
+    ---@module "auto-session"
+    ---@type AutoSession.Config
+    opts = {
+      suppressed_dirs = { '~/', '~/Projects', '~/Downloads', '/' },
+      -- log_level = 'debug',
+    },
+  },
 }
